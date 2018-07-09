b8f7077330c3b15b0ce8b7987b8b694c0288651b
AmirrezaAsk <raskarpour@gmail.com>
2018-07-09 19:23:18 +0430

    emoji :), codes moved to functions

--- main.go
+++ main.go
@@ -1,39 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/mdp/qrterminal"
 )
 
-func main() {
+const ()
 
+func getIP() (string, error) {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
-		os.Exit(1)
+		return "", fmt.Errorf("could not get interface addrs :%v", err)
 	}
-	myIP := ""
 	for _, a := range addrs {
 		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				myIP = ipnet.IP.String()
+				return ipnet.IP.String(), nil
 			}
 		}
 	}
+	return "", errors.New("could not find ip")
+}
+
+func main() {
+	//get file name
 	if len(os.Args) < 2 {
 		log.Fatalln("need a file name ")
 	}
 	fileName := os.Args[1]
+	//get ip
+	ip, err := getIP()
+	if err != nil {
+		log.Fatalf("could not get ip:%v", err)
+	}
+	//start a file server
 	fs := http.FileServer(http.Dir("."))
-	url := fmt.Sprintf("http://%s:7777/%s", myIP, fileName)
+	url := fmt.Sprintf("http://%s:7777/%s", ip, fileName)
+	color.Cyan("🎊 %s 🎊 ", url)
 	qrterminal.Generate(url, qrterminal.L, os.Stdout)
+	//start webserver
 	http.Handle("/", http.StripPrefix("/", fs))
-	log.Println("File URL: " + url)
 	if err := http.ListenAndServe(":7777", nil); err != nil {
 		log.Fatalf("could not create web server :%v", err)
 	}
